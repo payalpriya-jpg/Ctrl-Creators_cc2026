bd7d1b33147d40a6194c2c337b62f048e7efe633
hatiyadhawal <dhawal.hatiya@avantika.edu.in>
2026-05-13 11:55:04 +0530

    Adding serving plate for Jalebi Stack base

--- JalebiStack.js
+++ JalebiStack.js
@@ -11,3 +11,14 @@ function draw() {
 
   background(255, 228, 180);
 }
+
+function drawPlate() {
+
+  fill(230);
+
+  ellipse(width / 2, 540, 420, 70);
+
+  fill(245);
+
+  ellipse(width / 2, 530, 380, 45);
+}
